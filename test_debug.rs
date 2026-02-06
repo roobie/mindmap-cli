37153efcd346ec4a6cfbee282596a7d2dcda5ae0
bjorn <bjorn.roberg@gmail.com>
2026-02-06 14:29:14 +0100

    refactor: Remove obsolete test files and associated code

--- test_debug.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let path = std::path::Path::new("/etc/passwd");
-    println!("is_absolute: {}", path.is_absolute());
-    
-    for component in path.components() {
-        println!("component: {:?}", component);
-    }
-}
